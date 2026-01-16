ff06b34254b45e99667373d6d7ab58d1da5ec9b5
Ahmed <medosamsam919@gmail.com>
2026-01-16 21:56:47 +0200

    Fixing Bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { supabase } from "./lib/Supabase";
 import ComingSoonPage from "./components/UnderConstructionPage";
 
 export default async function EntryLayout() {
-  const headersList = await headers();
-  const cookieHeader = headersList.get("cookie") || "";
-  const match = cookieHeader.match(/user_id=([^;]+)/);
-  const userId = match ? match[1] : null;
+  // const headersList = await headers();
+  // const cookieHeader = headersList.get("cookie") || "";
+  // const match = cookieHeader.match(/user_id=([^;]+)/);
+  // const userId = match ? match[1] : null;
 
-  if (!userId) return <ComingSoonPage />;
+  // if (!userId) return <ComingSoonPage />;
 
-  const { data: user, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("id", userId)
-    .single();
+  // const { data: user, error } = await supabase
+  //   .from("users")
+  //   .select("*")
+  //   .eq("id", userId)
+  //   .single();
 
-  if (!user || error) return <ComingSoonPage />;
+  
 
-  return redirect("/dashboard");
+  return <ComingSoonPage />;
 }
